a9f31a7cdcd87735888d03eb89336af3781ee080
Claudia Ziemba <claudia.ziemba@gmail.com>
2020-05-06 23:59:10 -0300

    Updates

---
+++ precache-manifest.ea4ddae4606f72e16324801d82a74919.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc3e7e2a47ed10d4a616",
+    "url": "/pokedex/static/css/main.cc5ebb7c.chunk.css"
+  },
+  {
+    "revision": "bc3e7e2a47ed10d4a616",
+    "url": "/pokedex/static/js/main.8b6f04d4.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "e6d9cade2f2632aad926",
+    "url": "/pokedex/static/js/2.4f3b3be1.chunk.js"
+  },
+  {
+    "revision": "04a2807948bb8c80e7ecba3259723352",
+    "url": "/pokedex/index.html"
+  }
+];
